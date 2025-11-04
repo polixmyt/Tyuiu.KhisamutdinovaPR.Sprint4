2889aca0b1955959ae59673b65a57ada3e43031e
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-11-04 19:41:57 +0500

    Task2

---
+++ Tyuiu.KhisamutdinovaPR.Sprint4.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tyuiu.MargaryanSG.Sprint4.Task2.V5.Lib
+{
+    public class DataService
+    {
+        public int Calculate(int[] array)
+        {
+            int product = 1;
+            bool hasEven = false;
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0) // проверка на четность
+                {
+                    product *= num;
+                    hasEven = true;
+                }
+            }
+
+            if (!hasEven)
+            {
+                return 0; // если нет четных элементов
+            }
+
+            return product;
+        }
+    }
+}
---
+++ Tyuiu.KhisamutdinovaPR.Sprint4.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MargaryanSG.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.MargaryanSG.Sprint4.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 4, 3, 6, 5, 8, 7, 9, 4, 3, 6, 8, 5, 4, 7 };
+            int result = ds.Calculate(array);
+            int wait = 4 * 6 * 8 * 4 * 6 * 8 * 4; // произведение четных элементов
+
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCalculateNoEven()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 3, 5, 7, 9, 3, 5, 7, 9, 3, 5, 7, 9, 3, 5 };
+            int result = ds.Calculate(array);
+            int wait = 0; // нет четных элементов
+
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithOneEven()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 3, 5, 7, 4, 3, 5, 7, 9, 3, 5, 7, 9, 3, 5 };
+            int result = ds.Calculate(array);
+            int wait = 4; // только один четный элемент
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
